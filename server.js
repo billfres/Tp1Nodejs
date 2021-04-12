3b053ec102788f889ef19b8d333a5ffb8822abe7
billfres <ngomafresnel@gmail.com>
2021-04-12 18:08:30 +0200

    Suite de la configuration du server

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 //Dans la dernière ligne, vous configurez le serveur pour qu'il écoute 
 const http = require('http');
+const app = require('./app');
 
-const server = http.createServer((req, res) => {
-    res.end('Voilà la réponse du serveur !');
-});
+app.set('port', process.env.PORT || 3000);
+const server = http.createServer(app);
+
+
 
 server.listen(process.env.PORT || 3000);
 
