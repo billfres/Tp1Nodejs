e6de91b5b908704c2257dafb2bb992541d26bcfa
billfres <ngomafresnel@gmail.com>
2021-04-12 18:56:05 +0200

    ajout des headers dans app.js pour corriger les bugs

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const express = require('express');
 
 const app = express();
+/*
+Ces headers permettent :
+    d'accéder à notre API depuis n'importe quelle origine ( '*' ) ;
+    d'ajouter les headers mentionnés aux requêtes envoyées vers notre API (Origin , X-Requested-With , etc.) ;
+    d'envoyer des requêtes avec les méthodes mentionnées ( GET ,POST , etc.).
+*/
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
 app.use('/api/stuff', (req, res, next) => {
     const stuff = [
       {
