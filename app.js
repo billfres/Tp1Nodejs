37dd39d37395ea9de829b47a73322fbaf889a3ff
billfres <ngomafresnel@gmail.com>
2021-04-13 16:33:46 +0200

    Ajout de la route pour modifier une seule donnée

--- app.js
+++ app.js
@@ -59,4 +59,11 @@ app.get('/api/stuff/:id', (req, res, next) => {
       .catch(error => res.status(404).json({ error }));
   });
 
+  //la route pour modifier une donnée ou un objet
+app.put('/api/stuff/:id', (req, res, next) => {
+    Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+      .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+      .catch(error => res.status(400).json({ error }));
+  });
+
 module.exports = app;
