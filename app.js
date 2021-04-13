b006d992ec5548b561afdfe15e645d4008016e6a
billfres <ngomafresnel@gmail.com>
2021-04-13 18:12:15 +0200

    Configuration du routage

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const express = require('express');
 //importation du package body-parser
 const bodyParser = require('body-parser');
+//importation du package mongoose
+const mongoose = require('mongoose');
 
-//importation du model thing
-const Thing = require('./models/Thing');
+//importation du routeur
+const stuffRoutes = require('./routes/stuff');
+
 
 const app = express();
 
-//importation du package mongoose
-const mongoose = require('mongoose');
+
 
 //configuration de la BD
 mongoose.connect('mongodb+srv://fresneltest1:fresneltest1@cluster0.xxdoh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
@@ -34,44 +36,7 @@ app.use((req, res, next) => {
 //app.use(body-parser.json());
 app.use(express.json());
 
-//ce middleware ne traite que les requêtes de types POST
-app.post('/api/stuff', (req, res, next) => {
-    delete req.body._id;
-    const thing = new Thing({
-      ...req.body
-    });
-    thing.save()
-      .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
-      .catch(error => res.status(400).json({ error }));
-  });
-
-//notre route de get pour reccuperer les données: get or use
-app.get('/api/stuff', (req, res, next) => {
-    Thing.find()
-    .then(things => res.status(200).json(things))
-    .catch(error => res.status(400).json({ error }));
-  });
-
-//la route pour modifier une donnée ou un objet
-app.put('/api/stuff/:id', (req, res, next) => {
-    Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-      .then(() => res.status(200).json({ message: 'Objet modifié !'}))
-      .catch(error => res.status(400).json({ error }));
-  });
-
-  //Route pour supprimer une donnée
-app.delete('/api/stuff/:id', (req, res, next) => {
-    Thing.deleteOne({ _id: req.params.id })
-      .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
-      .catch(error => res.status(400).json({ error }));
-  });
-//la route de get pour reccuperer une seule donnée
-app.get('/api/stuff/:id', (req, res, next) => {
-    Thing.findOne({ _id: req.params.id })
-      .then(thing => res.status(200).json(thing))
-      .catch(error => res.status(404).json({ error }));
-  });
-
-
+//completion de la route avec staffRoutes
+app.use('/api/stuff', stuffRoutes);
 
 module.exports = app;
