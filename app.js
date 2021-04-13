90833217413ba907400d1c83dac53c5fa10adf82
billfres <ngomafresnel@gmail.com>
2021-04-13 13:06:12 +0200

    ajout du package body-parser pour transformer les donnees en json

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+//importation du package body-parser
+const bodyParser = require('body-parser');
 
 const app = express();
 /*
@@ -12,6 +14,17 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
+  });
+
+//définissons sa fonction json comme middleware global pour votre application, juste après avoir défini les headers de la réponse :
+app.use(express.json());
+
+//ce middleware ne traite que les requêtes de types POST
+app.post('/api/stuff', (req, res, next) => {
+    console.log(req.body);
+    res.status(201).json({
+      message: 'Objet créé !'
+    });
   });
 
 app.use('/api/stuff', (req, res, next) => {
