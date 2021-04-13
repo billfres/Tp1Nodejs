a4c7464e0316aa4218635fd4893290c0ad24dbaa
billfres <ngomafresnel@gmail.com>
2021-04-13 16:40:31 +0200

    Ajout de la route pour supprimer une seule donnée

--- app.js
+++ app.js
@@ -52,6 +52,19 @@ app.get('/api/stuff', (req, res, next) => {
     .catch(error => res.status(400).json({ error }));
   });
 
+//la route pour modifier une donnée ou un objet
+app.put('/api/stuff/:id', (req, res, next) => {
+    Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+      .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+      .catch(error => res.status(400).json({ error }));
+  });
+
+  //Route pour supprimer une donnée
+app.delete('/api/stuff/:id', (req, res, next) => {
+    Thing.deleteOne({ _id: req.params.id })
+      .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
+      .catch(error => res.status(400).json({ error }));
+  });
 //la route de get pour reccuperer une seule donnée
 app.get('/api/stuff/:id', (req, res, next) => {
     Thing.findOne({ _id: req.params.id })
@@ -59,11 +72,6 @@ app.get('/api/stuff/:id', (req, res, next) => {
       .catch(error => res.status(404).json({ error }));
   });
 
-  //la route pour modifier une donnée ou un objet
-app.put('/api/stuff/:id', (req, res, next) => {
-    Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-      .then(() => res.status(200).json({ message: 'Objet modifié !'}))
-      .catch(error => res.status(400).json({ error }));
-  });
+
 
 module.exports = app;
